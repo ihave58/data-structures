03b5d0759fbab4691d9e3a05b43da37ddfc5b09a
Ady <ihave58@yahoo.com>
2025-01-27 14:25:03 +0530

    Solves Easy Sodoku. Need to work on medium and hard levels.

--- sodoku.js
+++ sodoku.js
@@ -1,15 +1,79 @@
+const getPossibleSets = (board, rowSets, colSets, groupSets) => {
+    const possibleSets = [];
+    const allPossibleNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+        const possibleRowSets = [];
+
+        for (let colIndex = 0; colIndex < board[rowIndex].length; colIndex++) {
+            if (board[rowIndex][colIndex] === '.') {
+                const groupRowIndex = Math.floor(rowIndex / 3);
+                const groupColIndex = Math.floor(colIndex / 3);
+
+                const possibleCellSet = new Set(allPossibleNumbers);
+
+                for (let value of rowSets[rowIndex].values()) {
+                    possibleCellSet.delete(value);
+                }
+
+                for (let value of colSets[colIndex].values()) {
+                    possibleCellSet.delete(value);
+                }
+
+                for (let value of groupSets[groupRowIndex][groupColIndex].values()) {
+                    possibleCellSet.delete(value);
+                }
+
+                possibleRowSets.push(possibleCellSet.size === 0 ? null : possibleCellSet);
+            } else {
+                possibleRowSets.push(null);
+            }
+        }
+
+        possibleSets.push(possibleRowSets);
+    }
+
+    return possibleSets;
+}
+
+const solveEachCell = (board, rowSets, colSets, groupSets, possibleSets) => {
+    for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+        for (let colIndex = 0; colIndex < board[rowIndex].length; colIndex++) {
+            if (possibleSets[rowIndex][colIndex] !== null && possibleSets[rowIndex][colIndex].size === 1) {
+                const groupRowIndex = Math.floor(rowIndex / 3);
+                const groupColIndex = Math.floor(colIndex / 3);
+
+                const value = board[rowIndex][colIndex] = Array.from(possibleSets[rowIndex][colIndex].values())[0];
+
+                rowSets[rowIndex].add(value);
+                colSets[colIndex].add(value);
+                groupSets[groupRowIndex][groupColIndex].add(value)
+            }
+        }
+    }
+}
+
+const hasPossibleSolution = (board, possibleSets) => {
+    for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
+        for (let colIndex = 0; colIndex < board[rowIndex].length; colIndex++) {
+            if (possibleSets[rowIndex][colIndex] !== null && possibleSets[rowIndex][colIndex].size === 1) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
 const solveSudoku = function (board) {
     const rowSets = [];
     const colSets = [];
-    const allPossibleNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
     const groupSets = [
         [new Set(), new Set(), new Set()],
         [new Set(), new Set(), new Set()],
         [new Set(), new Set(), new Set()]
     ];
-
-    const possibleSets = [];
 
     for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
         const rowSet = new Set();
@@ -56,57 +120,42 @@ const solveSudoku = function (board) {
         }
     }
 
-    for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
-        const possibleRowSets = [];
+    let possibleSets = getPossibleSets(board, rowSets, colSets, groupSets);
 
-        for (let colIndex = 0; colIndex < board[rowIndex].length; colIndex++) {
-            if (board[rowIndex][colIndex] === '.') {
-                const possibleCellSet = new Set(allPossibleNumbers);
+    while (hasPossibleSolution(board, possibleSets)) {
+        solveEachCell(board, rowSets, colSets, groupSets, possibleSets);
 
-                rowSets[rowIndex].values().forEach((value) => possibleCellSet.delete(value));
-                colSets[colIndex].values().forEach((value) => possibleCellSet.delete(value));
-
-                const groupRowIndex = Math.floor(rowIndex / 3);
-                const groupColIndex = Math.floor(colIndex / 3);
-
-                groupSets[groupRowIndex][groupColIndex].values().forEach((value) => possibleCellSet.delete(value));
-
-                possibleRowSets.push(possibleCellSet);
-            } else {
-                possibleRowSets.push('.');
-            }
-        }
-
-        possibleSets.push(possibleRowSets);
-    }
-
-    for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
-        for (let colIndex = 0; colIndex < board[rowIndex].length; colIndex++) {
-            if (possibleSets[rowIndex][colIndex].size === 1) {
-                board[rowIndex][colIndex] = possibleSets[rowIndex][colIndex].values().toArray()[0];
-
-                possibleSets[rowIndex][colIndex] = ".";
-                rowSets[rowIndex].add(board[rowIndex][colIndex]);
-                colSets[colIndex].add(board[rowIndex][colIndex]);
-            }
-        }
+        possibleSets = getPossibleSets(board, rowSets, colSets, groupSets);
     }
 
     // console.log(rowSets, colSets, groupSets);
-    console.dir(board);
+    console.table(board);
+    console.dir(possibleSets);
 };
 
 
+// const board = [
+//     ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+//     ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+//     ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+//     ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+//     ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+//     ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+//     ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+//     ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+//     ['.', '.', '.', '.', '8', '.', '.', '7', '9']
+// ];
+
 const board = [
-    ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
-    ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
-    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
-    ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
-    ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
-    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
-    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
-    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
-    ['.', '.', '.', '.', '8', '.', '.', '7', '9']
+    ['.', '.', '9', '7', '4', '8', '.', '.', '.'],
+    ['7', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '2', '.', '1', '.', '9', '.', '.', '.'],
+    ['.', '.', '7', '.', '.', '.', '2', '4', '.'],
+    ['.', '6', '4', '.', '1', '.', '5', '9', '.'],
+    ['.', '9', '8', '.', '.', '.', '3', '.', '.'],
+    ['.', '.', '.', '8', '.', '3', '.', '2', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '6'],
+    ['.', '.', '.', '2', '7', '5', '9', '.', '.']
 ];
 
 solveSudoku(board)
